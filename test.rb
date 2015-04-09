13cde348ab70cd34f6e6cfee62dcf5e30d916913
Mark Noble <noble.m.a@gmail.com>
2015-04-09 21:47:06 +0800

    AES part

--- test.rb
+++ test.rb
@@ -1,51 +1,12 @@
+require 'openssl'
+require 'json'
+
 document = "4916603231464963 Mar-30-2020 Soumya Ray Visa"
-ciphertext = "Su o0022aiVsa Ry yma93466134026369413--0rM a"
-key = 3
+key = "some text, how long"
 
-'''doc = document.to_s.chars.map(&:ord)
-docLen = doc.length
-sqrDoc = (Math.sqrt(docLen)).ceil
-
-puts sqrDoc
-
-'''
-    cipher_length=ciphertext.to_s.length
- if  cipher_length % Math.sqrt(cipher_length) == 0
-        cipher_row = Math.sqrt(cipher_length).to_i
-        cipher_column= cipher_row
-      else
-        cipher_row = Math.sqrt(cipher_length).ceil
-        cipher_column= cipher_row + 1
-      end
-
-      seq=(0..cipher_length-1).to_a.map{|element|element.to_s}
-      seq=seq.each_slice(cipher_column).to_a
-      seq=seq.shuffle(random: Random.new(key) )
-      seq=seq.collect! {|ele| ele.shuffle(random: Random.new(key))}
-      seq=seq.flatten.map{|index| index.to_i}
-
-      doc=ciphertext.to_s.chars.to_a
-      newt = []
-      count = 0
-
-      doc.each do
-      	newt << doc[seq.index(count)]
-#      	puts newt, seq.index(count)
-      	count += 1
-      end
-
-      puts newt.join
-
-
-
-#      together = doc.zip(seq)
-      #puts together.join(" ")
- #     final = together.sort_by{|x,y| y}
-      #finals = final.map {|x,y| x}
-
-  #    puts final.join("")
-
-      #sorted = doc.sort_by { |k| k["value"] }
-      #cipher=ciphertext.to_s.chars.to_a
-      #puts cipher.join
-
+cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    cipher.encrypt
+    cipher.key = key.to_s
+    iv = cipher.random_iv
+    ciphertext = cipher.update(document.to_s) + cipher.final
+    puts [iv, ciphertext].map(){|s| s.unpack('H*')[0]}.to_json
--- test1.rb
+++ test1.rb
@@ -1,40 +1,17 @@
+require 'openssl'
+require 'json'
+
 document = "4916603231464963 Mar-30-2020 Soumya Ray Visa"
-fcipher = "2020 SouVisamya Ray 3146496349166032 Mar-30-"
-scipher = "Su o0022aiVsa Ry yma93466134026369413--0rM a"
-key = 3
+aes_crypt = "2c5f6f7a378978eb805f3bbf3162a18d","358f759a6377619f3887eb073ce905ab5c13067b63d88c1105fa702ac1e668108623f2f9d62f8fbf0e81796e960c44da"
+key = "some text, how long"
 
-'''
-class Array
-  def unshuffle(random:)
-    transformed_order = (0...length).to_a.shuffle!(random: random)
-    sort_by.with_index{|_, i| transformed_order[i]}
-  end
-end
-'''
 
-    doc_length=document.to_s.length
-    # 1. find number of rows/cols such that matrix is almost square
-    if  doc_length % Math.sqrt(doc_length) == 0
-        doc_row = Math.sqrt(doc_length).to_i
-        doc_column= doc_row
-      else
-        doc_row = Math.sqrt(doc_length).ceil
-        doc_column= doc_row + 1
-      end
- puts document
-    # 2. break plaintext into evenly sized blocks
-    doc=document.to_s.chars.each_slice(doc_column).to_a
-    ## have seq of transpose
-    seq=(0..doc_length-1).to_a.map{|element|element.to_s}
-    seq=seq.each_slice(doc_column).to_a
-    ##
-    # 3. sort rows in predictibly random way using key as seed
-    doc=doc.shuffle(random: Random.new(key) )
-    seq=seq.shuffle(random: Random.new(key) )
-    
-    # 4. sort columns of each row in predictibly random way
-    doc=doc.collect! {|ele| ele.shuffle(random: Random.new(key))}
-    seq=seq.collect! {|ele| ele.shuffle(random: Random.new(key))}
-    #puts doc.join, seq.join(" ")
-    # 5. return joined cyphertext
-  puts doc.join
+iv, ciphertext =  JSON.parse(aes_crypt).map {|s| [s].pack("H*")}
+
+    decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    decipher.decrypt
+    decipher.key = key
+    decipher.iv = iv
+    plain = decipher.update(ciphertext) + decipher.final
+    return plain
+
